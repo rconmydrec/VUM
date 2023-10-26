57ef8267ef06a79b7a78ad878e012e93715a9035
RconMydrec <rconmydrec@gmail.com>
2023-10-26 11:55:05 +0300

    programs add

---
+++ CorrectID/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+public class Program
+{
+    public static bool IsCorrectID(string id)
+    {
+        return id.Length == 10
+               && long.TryParse(id, out _)
+               && DateTime.TryParseExact(id.Substring(0, 6), "yyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+    }
+
+    public static void Main()
+    {
+        string[] testIDs =
+        {
+            "6006307744",
+            "0112088762",
+            "9902286654",
+            "2312318877",
+            "3654104321",
+            "za456789"
+        };
+
+        foreach (string testID in testIDs)
+        {
+            Console.WriteLine($"{testID} is {(IsCorrectID(testID) ? "correct" : "incorrect")} Bulgarian ID");
+        }
+    }
+}
---
+++ StartingCode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arrNames = new string[5];
+        arrNames[0] = "Vesso";
+        arrNames[1] = "Alan Bebera";
+        arrNames[2] = "Ardita123";
+        arrNames[3] = "bebra";
+        arrNames[4] = "amogusыы";
+
+        foreach (string name in arrNames)
+        {
+            if (IsValidName(name))
+            {
+                Console.WriteLine($"{name} - Valid Name");
+            }
+            else
+            {
+                Console.WriteLine($"{name} - Invalid Name");
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsValidName(string name)
+    {
+        for (int i = 0; i < name.Length; i++)
+        {
+            char c = name[i];
+            if (!Char.IsLetter(c) && !Char.IsWhiteSpace(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
